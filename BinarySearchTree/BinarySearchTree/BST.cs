58897f21957af05ff4112bb3a77d94adc056a17c
roshni1015 <roshniadatrao1110@gmail.com>
2022-05-14 11:58:38 +0530

    [Roshni]-Getting Size

--- BinarySearchTree/BinarySearchTree/BST.cs
+++ BinarySearchTree/BinarySearchTree/BST.cs
@@ -6,56 +6,61 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTree
 {
-    public class BST<R> where R : IComparable<R>
-    {
-        
-        public R NodeData { get; set; }
-        public BST<R> LeftTree { get; set; }
-        public BST<R> RightTree { get; set; }
-        public BST(R nodeData)
-        {
-            this.NodeData = nodeData;
-            this.LeftTree = LeftTree;
-            this.RightTree = RightTree;
-        }
-        int leftCount = 0, rightCount = 0;
-        bool result = false;
-        public void Add(R item)
-        {
-            R currentNodeValue = this.NodeData;
-            if (currentNodeValue.CompareTo(item) > 0)
-            {
-                if (this.LeftTree == null)
-                    this.LeftTree = new BST<R>(item);
-                else
-                    this.LeftTree.Add(item);
-            }
-            else
-            {
-                if (this.RightTree == null)
-                    this.RightTree = new BST<R>(item);
-                else
-                    this.RightTree.Add(item);
-            }
-        }
+     public class BST<R> where R : IComparable<R>
+     {
 
-        public void Display()
-        {
-            if (this.LeftTree != null)
-            {
-                this.leftCount++;
-                this.LeftTree.Display();
+         public R NodeData { get; set; }
+         public BST<R> LeftTree { get; set; }
+         public BST<R> RightTree { get; set; }
+         public BST(R nodeData)
+         {
+             this.NodeData = nodeData;
+             this.LeftTree = LeftTree;
+             this.RightTree = RightTree;
+         }
+         int leftCount = 0, rightCount = 0;
+         bool result = false;
+         public void Add(R item)
+         {
+             R currentNodeValue = this.NodeData;
+             if (currentNodeValue.CompareTo(item) > 0)
+             {
+                 if (this.LeftTree == null)
+                     this.LeftTree = new BST<R>(item);
+                 else
+                     this.LeftTree.Add(item);
+             }
+             else
+             {
+                 if (this.RightTree == null)
+                     this.RightTree = new BST<R>(item);
+                 else
+                     this.RightTree.Add(item);
+             }
+         }
 
-            }
-            Console.WriteLine(this.NodeData.ToString());
-            if (this.RightTree != null)
-            {
-                this.rightCount++;
-                this.RightTree.Display();
-            }
-        }
-       
-        
-    }
-}
+         public void Display(BST<R> bst)
+         {
+             if (this.LeftTree != null)
+             {
+                 bst.leftCount++;
+                 this.LeftTree.Display(bst);
+
+             }
+             Console.WriteLine(this.NodeData);
+             if (this.RightTree != null)
+             {
+                 bst.rightCount++;
+                 this.RightTree.Display(bst);
+             }
+         }
+         public void GetSize(BST<R> bst)
+         {
+             Console.WriteLine("Size" + " " + (1 + this.leftCount + this.rightCount));
+         }
+
+
+     }
+ }
+   
 
